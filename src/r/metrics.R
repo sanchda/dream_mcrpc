78a6a1c753260c77f44c9de625c70f22a9ed91c9
David Sanchez <sanchda@gmail.com>
2015-04-20 13:54:14 -0400

    General updates integrating EN with other stuff
    Thanks joe!

--- src/r/metrics.R
+++ src/r/metrics.R
@@ -1,25 +1,14 @@
 #Judging how well everything works
 
-
-
-
 #At what times am I basing my estimates?
 
-
-
-
 #What are my risk scores at time T
-V = estvector
-
-times = c(12*30)
-
-
-
-
-
-delta = censor[,1]
-
-time = survive[,1]
+showSurvMetrics = function(months, delta, time, V) {
+  library('timeROC');
+  times = c(months*30);
 
 Q = timeROC(time,delta,V,other_markers = NULL,1, weighting = 'marginal',times)
 
+return(Q);
+
+}
