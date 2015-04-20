d28cee6c89f9de01590426fde385be38d8206af6
David Sanchez <sanchda@gmail.com>
2015-04-20 12:24:03 -0400

    Organize driver to start testing

--- src/r/driver.r
+++ src/r/driver.r
@@ -1,6 +1,9 @@
 library(R.matlab)
 
 # Load and format data
+featNames22 = readMat('../../data/halabi_22_feat_names.mat');
+featNames22 = featNames22$f22.feat.names;
+featNames22 = unlist( featNames22 );
 survival = readMat('../../data/cleaned_surv.mat');
 censor   = readMat('../../data/cleaned_censor.mat');
 features = readMat('../../data/cleaned_ind.mat');
@@ -21,3 +24,11 @@ time = survival$surv.var;
 delta = censor$censoring;
 z = features22$ind.var22;
 lambda = 1;
+
+# Organize variables to test model
+
+X = features22$ind.var22;
+t = survival$surv.var;
+coeffs = result[2,];
+names(coeffs) = featNames22;
+names(X) = featNames22;
