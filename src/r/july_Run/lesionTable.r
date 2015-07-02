2a22aa07e085d1e2e1a28ad1dde7394b461a30cd
David Sanchez <sanchda@gmail.com>
2015-07-02 17:46:23 -0400

    Completed vitalsTable Run this function to receive an output, coreTable2, which merges coreTable with vitals.

--- src/r/july_Run/lesionTable.r
+++ src/r/july_Run/lesionTable.r
@@ -1,4 +1,4 @@
 coreTable = read.csv("data\\raw_data\\CoreTable_training.csv", stringsAsFactors=FALSE);
-lesTable  = read.csv("data\\raw_data\\LesionMeasure_training.csv", stringsAsFactors=FALSE);
+lesTable  = read.csv("data\\raw_data\\LesionMeasure_training.csv", stringsAsFactors=FALSE, na.strings=c("NA","NaN", " ", "", "."));
 
 # Process 
